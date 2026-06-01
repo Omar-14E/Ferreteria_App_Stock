1c53f76b83500eaed7d98cc7732ecea3a9b0443c
its-Noemi <noeliacusquisiban@gmail.com>
2026-05-31 23:11:40 -0500

    Feat: titulo cambiado a Deshboard de Ineventario

--- lib/features/home/home_screen.dart
+++ lib/features/home/home_screen.dart
@@ -22,7 +22,7 @@ class HomeScreen extends StatelessWidget {
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             const Text(
-              'Panel de Control',
+              'Dashboard Inventario',
               style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
             ),
             const SizedBox(height: 5),
